04a1b7101ab1bc975f3c1f6c126d26b00ba970a2
unknown <tania.bhattacharaya@msds.christuniversity.in>
2023-01-28 12:15:54 +0530

    c1

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class hello {  // defining a class class classname
     public static void main(String [] args){  //main method similar to function void no return (an array of string , stored in variable args)
         System.out.println("Helle...!");
+        System.out.println("Helllllllll!");
     }
 }
